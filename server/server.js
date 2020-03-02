79e8d03ae6151aabe1c219b2631f99af8fbaa4a6
Jack Zheng <jackzheng10@yahoo.com>
2020-03-02 14:52:03 -0500

    added path require

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 let routes = require("./routes");
 
 require("dotenv").config();
