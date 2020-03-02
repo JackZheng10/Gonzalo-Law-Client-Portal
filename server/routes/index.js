07246d8c17db6db0b75f0ca30ddaacc913c41e5c
Jack Zheng <jackzheng10@yahoo.com>
2020-03-02 17:35:17 -0500

    default page for heroku fixes pt 3

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const routes = express.Router();
-const { users } = require("../controllers");
 const { login, register } = require("../controllers");
 
-routes.get("/", users);
 routes.post("/login", login);
 routes.post("/register", register);
 
