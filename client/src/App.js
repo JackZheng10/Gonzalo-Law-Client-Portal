be8e2c84c03f0f10b9e88ab68bde2263437d976f
Jack Zheng <jackzheng10@yahoo.com>
2020-03-02 17:22:37 -0500

    default page for heroku fixes

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Login from "./components/login";
 import Register from "./components/register";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Dashboard from "./components/dashboard";
 
 export default class App extends Component {
@@ -15,10 +15,13 @@ export default class App extends Component {
       <React.Fragment>
         <Header />
         <Switch>
-          <Route exact path="/" component={Welcome} />
+          <Route exact path="/Welcome" component={Welcome} />
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
           <Route path="/dashboard" component={Dashboard} />
+          <Route exact path="/">
+            <Redirect to="/Welcome" />
+          </Route>
         </Switch>
         <br />
       </React.Fragment>
