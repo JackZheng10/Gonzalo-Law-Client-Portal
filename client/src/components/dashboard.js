a82576465efd48eb08baa89f99c466a32e42f563
chensation <fogwhite99@gmail.com>
2020-03-03 22:04:02 -0500

    Added modal for adding new project. Not finalized.

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
+import {Link} from "react-router-dom";
 // import axios from 'axios';
 
 export default class dashboard extends Component {
+
   state = {
     user: "",
     name: "",
-    persons: ["email", "name", "password"]
-  };
+    persons: ["email", "name", "password"],
+    data:[
+     {
+       name: "example 1",
+       type: "type 1"
+     },
+     {
+       name: "example 2",
+       type: "type 2"
+     },
+     {
+       name: "example 3",
+       type: "type 3"
+     },
+     {
+       name: "example 4",
+       type: "type 4"
+     },
 
+   ],
+
+  };
   componentDidMount() {
     const user = localStorage.getItem("user");
-    const name = localStorage.getItem("name"); //currently not used, since not set in login component
+    const name = localStorage.getItem("name");
+ //currently not used, since not set in login component
     this.setState({
       user: user,
       name: name
@@ -18,6 +40,7 @@ export default class dashboard extends Component {
   }
 
   render() {
+
     return (
       <div>
         <div className="col-md-6 m-auto">
@@ -30,7 +53,11 @@ export default class dashboard extends Component {
               <strong>{this.state.user}</strong>
             </h1>
             <br />
-            <a className="btn" href="/projects"> Projects</a>
+            <Link className="btn btn-danger btn-block" to={{
+                                      pathname:"/projects",
+                                      state: {
+                                        data: this.state.data}
+                                      }}> Projects</Link>
             <br />
             <a className="btn btn-danger btn-block" href="/login">
               Logout{" "}
