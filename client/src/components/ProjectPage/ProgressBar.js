558e2e70fbe5e88d63e7ccf32aa0b81f35d5e892
chensation <fogwhite99@gmail.com>
2020-03-30 00:46:10 -0400

    prevent normal user from incrementing the progress

--- client/src/components/ProjectPage/ProgressBar.js
+++ client/src/components/ProjectPage/ProgressBar.js
@@ -3,6 +3,7 @@ import CDPhases from "../../enums/CDPhases.js";
 import DNPhases from "../../enums/DNPhases.js";
 import IPPhases from "../../enums/IPPhases.js";
 import { Progress, Button, Popup } from 'semantic-ui-react';
+import jwtDecode from "jwt-decode";
 import axios from "axios";
 import baseURL from "../../baseURL";
 import "./ProgressBar.css";
@@ -62,6 +63,12 @@ const ProgressBar = (props)=>{
       });
   };
 
+  const incrementButton = ()=>{
+    const data = jwtDecode(localStorage.getItem("token"));
+
+    if (data.isAdmin)
+      return   <Button onClick={increment}>Next Phase</Button>;
+  }
   let list = [];
 
   for(var i = 0; i <= maxPhase(); i++){
@@ -77,6 +84,7 @@ const ProgressBar = (props)=>{
   const width = 100/(maxPhase() + 1) +'%';
   document.documentElement.style.setProperty('--pbwidth', width);
 
+
   return(
     <div>
 
@@ -86,9 +94,7 @@ const ProgressBar = (props)=>{
         </ul>
       </div>
       <h3>{"Current Phase: "+phases()[props.phase]} </h3>
-      <Button onClick={increment}>Next Phase</Button>
-
-
+      {incrementButton()}
     </div>
   )
 
